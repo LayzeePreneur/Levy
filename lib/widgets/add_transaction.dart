42ca3961af4b1a58096f04d25df46b00d61e281c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-08-03 23:42:08 +0600

    Fix new transaction modal's bottom viewinset which arise on clicking textfield

--- lib/widgets/add_transaction.dart
+++ lib/widgets/add_transaction.dart
@@ -44,67 +44,74 @@ class _AddTransactionState extends State<AddTransaction> {
       ).then((date) => setState(() => _selectedDate = date));
     }
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      child: Column(
-        children: <Widget>[
-          TextField(
-            decoration: addTxField('Title'),
-            controller: titleController,
-            onSubmitted: (_) => _addTx(),
-          ),
-          SizedBox(height: 15),
-          TextField(
-            decoration: addTxField('Amount'),
-            keyboardType: TextInputType.number,
-            controller: amountController,
-            onSubmitted: (_) => _addTx(),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                _selectedDate == null
-                    ? 'No Date Choosen'
-                    : 'Picked Date: ${DateFormat('dd/MM/y').format(_selectedDate)}',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              FlatButton(
-                child: Text(
-                  'Choose Date',
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.only(
+          top: 45,
+          left: 25,
+          right: 25,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 45,
+        ),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: addTxField('Title'),
+              controller: titleController,
+              onSubmitted: (_) => _addTx(),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: addTxField('Amount'),
+              keyboardType: TextInputType.number,
+              controller: amountController,
+              onSubmitted: (_) => _addTx(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  _selectedDate == null
+                      ? 'No Date Choosen'
+                      : 'Picked Date: ${DateFormat('dd/MM/y').format(_selectedDate)}',
                   style: TextStyle(
                     fontSize: 17,
-                    fontWeight: FontWeight.w700,
-                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
+                FlatButton(
+                  child: Text(
+                    'Choose Date',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w700,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  onPressed: _startDatePicker,
+                ),
+              ],
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              child: RaisedButton(
+                child: Text(
+                  'Add Transaction',
+                  style: TextStyle(fontSize: 16),
+                ),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30),
                 ),
-                onPressed: _startDatePicker,
+                onPressed: _addTx,
               ),
-            ],
-          ),
-          Container(
-            alignment: Alignment.centerRight,
-            child: RaisedButton(
-              child: Text(
-                'Add Transaction',
-                style: TextStyle(fontSize: 16),
-              ),
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              onPressed: _addTx,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
